c700142b2753a814e8c1725218f0b98fef90f106
malca-mpo <malcampovirgenhugo.hv@gmail.com>
2024-08-07 18:16:53 -0700

    Create function updated

--- queries.c
+++ queries.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 MYSQL* connection(){
     MYSQL* con = mysql_init(NULL);
     if(!con){
@@ -19,42 +18,23 @@ MYSQL* connection(){
 }
 
 void create(MYSQL* con){
-    int numberColums;
-    char tableName[MAX_STRING];
-    char columName[MAX_STRING];
-    char columType[MAX_STRING];
-    char query[MAX_STRING * MAX_COLUMS];
 
-    printf("\nEnter the table name: ");
-    scanf(" %[^\n]", tableName);
-    printf("\nEnter the number of columns: ");
-    scanf("%d", &numberColums);
+    char name[MAX_STRING], lastname[MAX_STRING], query[MAX_QUERY];
 
-    // Start building the query
-    snprintf(query, sizeof(query), "CREATE TABLE %s (", tableName);
+  
+    printf("\nEnter the name: ");
+    scanf("%s", name);
+    printf("\nEnter last name: ");
+    scanf("%s", lastname);
 
-    for(int i = 0; i < numberColums; i++){
-      printf("\nEnter column %d name: ", i+1);
-      scanf(" %[^\n]", columName);
-
-      printf("\nEnter column %d type (e.g., INT, VARCHAR(100)): ", i+1);
-      scanf(" %[^\n]", columType);
-
-      if(i > 0){
-        strncat(query, ", ", sizeof(query) - strlen(query) - 1);
-      }
-        
-          snprintf(query + strlen(query), sizeof(query) - strlen(query), "%s %s", columName, columType);
-    }
-          strncat(query, ");", sizeof(query) - strlen(query));
-
-    // Print full query
-    printf("SQL Query: %s\n", query);
+    snprintf(query, sizeof(query), "INSERT INTO USER (NAME, LASTNAME) VALUES ('%s', '%s')", name, lastname);
 
     if(mysql_query(con, query)){
-        fprintf(stderr, "\nCREATE TABLE FAILED. Error: %s\n", mysql_error(con));
+      printf("\nOops!, something went wrong when inserting the data into database!\n");
+      mysql_close(con);
+      return;
     }else{
-        printf("\nTable create succesfully.\n");
+      printf("\nData enter into database succesfully!\n");
     }
-}
 
+}
